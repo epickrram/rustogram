de66f9c2829608173004df2c6051d67ff814b5e3
Mark Price <mark@epickrram.com>
2016-01-18 16:49:22 +0000

    fix unused var warnings

--- tests/data_access.rs
+++ tests/data_access.rs
@@ -10,11 +10,19 @@ fn test_get_total_count() {
     assert_eq!(20_000, histogram.get_total_count());
 }
 
+#[test]
+fn test_get_max_value() {
+    let histogram = get_histogram();
+    assert!(histogram.values_are_equivalent(100_000_000, histogram.get_max_value()));
+}
+
 fn get_histogram() -> rustogram::Histogram {
     let mut h = rustogram::new_histogram(HIGHEST_TRACKABLE_VALUE, NUMBER_OF_SIGNIFICANT_VALUE_DIGITS);
 
-    for i in 0..10_000 {
+    let mut i = 10_000;
+    while i != 0 {
         h.record_value_with_expected_interval(1_000, 10_000);
+        i -= 1;
     }
     h.record_value_with_expected_interval(100_000_000, 10_000);
     h
@@ -23,8 +31,10 @@ fn get_histogram() -> rustogram::Histogram {
 fn get_raw_histogram() -> rustogram::Histogram {
      let mut h = rustogram::new_histogram(HIGHEST_TRACKABLE_VALUE, NUMBER_OF_SIGNIFICANT_VALUE_DIGITS);
 
-    for i in 0..10_000 {
+    let mut i = 10_000;
+    while i != 0 {
         h.record_value(1_000);
+        i -= 1;
     }
     h.record_value(100_000_000);
     h
